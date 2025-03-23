df1e8f00169068d2a2c08656f7649f1ccc7f4c40
Matúš Konig <matuskonig@gmail.com>
2025-03-23 14:00:31 +0100

    complete testing framework

---
+++ README.md
@@ -0,0 +1,4 @@
+`uv venv venv --seed`
+
+`uv pip install . -e` for editable mode
+`uv pip install .` for casual install
---
+++ scripts/something.py
@@ -0,0 +1,13 @@
+from generative_multiagents import (
+    LLMAgent,
+    LLMBackend,
+    AgentModelBase,
+    ConversationManager,
+)
+
+
+print(LLMAgent)
+print(LLMBackend)
+print(AgentModelBase)
+print(AgentModelBase.model_json_schema())
+print(ConversationManager)
