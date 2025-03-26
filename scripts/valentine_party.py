f2d41926add63e994068ed018b34e732ddefb45f
Matúš Konig <matuskonig@gmail.com>
2025-03-26 09:35:09 +0100

    adjusted logging

--- scripts/valentine_party.py
+++ scripts/valentine_party.py
@@ -1,10 +1,12 @@
 from pydantic import BaseModel, Field
-
 import dotenv
 import os
-from typing import Literal
+from typing import Literal, override
 import networkx as nx
 from openai import AsyncOpenAI
+import numpy as np
+import asyncio
+import logging
 
 from generative_multiagents import (
     AgentModelBase,
@@ -13,10 +15,32 @@ from generative_multiagents import (
     SequentialConversationSelector,
     LLMAgent,
 )
-import numpy as np
-import asyncio
-import time
-from functools import cached_property, lru_cache
+
+
+class XMLExtraAdapter(logging.LoggerAdapter):
+    @override
+    def process(self, msg, kwargs):
+        if "extra" in kwargs and len(kwargs["extra"]) > 0:
+            content = "\n".join(
+                f"<{key}>\n{value}\n</{key}>" for key, value in kwargs["extra"].items()
+            )
+            kwargs["extra"]["content"] = "\n" + content
+        else:
+            kwargs["extra"] = {"content": ""}
+        return msg, kwargs
+
+
+def get_logger(file_name: str, level) -> logging.Logger:
+    logger = logging.Logger(file_name, level)
+
+    formatter = logging.Formatter(
+        "[%(asctime)s][%(levelname)s]: %(message)s%(content)s"
+    )
+    file_handler = logging.FileHandler(file_name, mode="w")
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    return XMLExtraAdapter(logger)
 
 
 # TODO: characteristiscs as description ?
@@ -42,12 +66,14 @@ class ExperimentData(BaseModel):
 
 
 async def main():
+    logger = get_logger("logs/valentine_party.log", level=logging.DEBUG)
+
     api_key = os.getenv("OPENAI_API_KEY") or None
     client = AsyncOpenAI(base_url=os.getenv("OPENAI_BASE_URL"), api_key=api_key)
     context = LLMBackend(
         client=client,
         model=os.getenv("OPENAI_COMPLETIONS_MODEL"),
-        temperature=1.5,
+        temperature=1.2,
         RPS=int(os.getenv("MAX_REQUESTS_PER_SECOND")),
     )
 
@@ -65,15 +91,12 @@ async def main():
     )
 
     await asyncio.gather(*[agent.get_agent_introduction_message() for agent in agents])
-    print("isabella")
-    print(await isabella.get_agent_introduction_message())
-    print()
-    print("maria")
-    print(await maria.get_agent_introduction_message())
-    print()
-    print("klaus")
-    print(await klaus.get_agent_introduction_message())
-    print()
+    logger.info("Agents initialized.")
+    for agent in agents:
+        logger.info(
+            f"Introducing {agent.data.full_name}",
+            extra={"introduction": await agent.get_agent_introduction_message()},
+        )
 
     conversation_selector = SequentialConversationSelector(
         structure=structure_graph,
@@ -81,22 +104,36 @@ async def main():
         initial_conversation=[(isabella, maria)],
     )
     manager = ConversationManager(
-        conversation_selector=conversation_selector, max_conversation_utterances=12
+        conversation_selector=conversation_selector,
+        max_conversation_utterances=12,
+        logger=logger,
     )
-    for i in range(6):
-        await manager.run_simulation()
+    for i in range(4):
+        await manager.run_simulation_epoch()
 
     question = "When is the party happening ? Did you hear about the party ?"
     results = asyncio.gather(*[agent.ask_agent(question) for agent in agents])
     for agent, answer in zip(agents, await results):
         print(f"{agent.data.full_name}: {answer}")
 
-    # print("isabella")
-    # isabella.print_memory()
-    # print("maria")
-    # maria.print_memory()
-    # print("klaus")
-    # klaus.print_memory()
+    print()
+    print("Usage statistics")
+    print(f"Total time: {context.total_time:.02f} s")
+    print(f"Completion tokens: {context.completion_tokens}")
+    print(f"Prompt tokens: {context.prompt_tokens}")
+    print(f"Tokens per second: {context.completion_tokens / context.total_time:.2f}")
+
+    def get_memory(agent: LLMAgent):
+        return "\n".join(
+            [
+                agent.memory.get_agent_memory(),
+                "Facts about others:",
+                agent.memory.dump_agents_knowledge(),
+            ]
+        )
+
+    for agent in agents:
+        logger.debug(agent.data.full_name, extra={"memory": get_memory(agent)})
 
 
 # TODO: memory compression
