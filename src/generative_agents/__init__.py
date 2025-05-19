2db99d5030771781d892c6b26ad8746e4f7f51c1
Matúš Konig <matuskonig@gmail.com>
2025-05-19 15:18:08 +0200

    adjusted __all__

--- src/generative_agents/__init__.py
+++ src/generative_agents/__init__.py
@@ -1,4 +1,12 @@
-from .agent import LLMAgent, AgentModelBase, default_builder, DefaultPromptBuilder
+from .agent import (
+    LLMAgent,
+    AgentModelBase,
+    default_builder,
+    DefaultPromptBuilder,
+    fixed_count_strategy_factory,
+    top_std_count_strategy_factory,
+    mean_std_count_strategy_factory,
+)
 from .llm_backend import LLMBackend
 from .conversation_managment import (
     ConversationManager,
@@ -13,7 +21,10 @@ __all__ = [
     "ConversationManager",
     "SequentialConversationSelector",
     "ConversationSelectorABC",
-    DefaultPromptBuilder,
-    default_builder
+    "DefaultPromptBuilder",
+    "default_builder",
+    "fixed_count_strategy_factory",
+    "top_std_count_strategy_factory",
+    "mean_std_count_strategy_factory",
 ]
 PACKAGE_VERSION = "1.0.0"
